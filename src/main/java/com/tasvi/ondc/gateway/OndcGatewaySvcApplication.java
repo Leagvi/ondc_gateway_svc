ed92fe9ff9073b8c165afa5012dbbf9777c1e55c
VenkataSaireddy_G <venkatasaireddy.g@gmail.com>
2024-10-05 22:07:18 +0530

    Initial commit

---
+++ src/main/java/com/tasvi/ondc/gateway/OndcGatewaySvcApplication.java
@@ -0,0 +1,13 @@
+package com.tasvi.ondc.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndcGatewaySvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OndcGatewaySvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tasvi/ondc/gateway/OndcGatewaySvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasvi.ondc.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OndcGatewaySvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
